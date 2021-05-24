a6c57e00d64a1d4d4881f6887aa988f084e30670
Orava2 <32876893+Orava2@users.noreply.github.com>
2021-05-24 11:07:16 +0300

    Create allowExecution.sh

---
+++ allowExecution.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Setting execution rights"
+
+chmod +x allowExecution.sh
+chmod +x script.sh
+chmod +x scriptOld.sh
+chmod +x scriptServer.sh
+chmod +x scriptStudent.sh
+chmod +x scriptStudent.sh
+chmod +x scriptUpdate.sh
+chmod +x usb-monster\dd-curses\write_dd.py
+
+echo "Done"
