5b6dabdf484bab82392ab7b62eb99fdb0e928248
Orava2 <32876893+Orava2@users.noreply.github.com>
2021-05-24 14:52:10 +0300

    Create usb-paahdin.sh

---
+++ usb-paahdin.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function menu {
+clear
+echo -ne "\e[1m\e[7m  ABITTI-tikkuohjelma aka USB-paahdin  \e[0m
+Valitse toiminto!
+1) Kirjoita opiskelijan tikuiksi
+2) Kirjoita palvelintikuiksi
+3) Kirjoita vanha versio
+4) Tarkista levykuvien p‰ivitykset
+5) Tarkista USB-paahtimen p‰ivitykset
+0) Lopeta\n"
+echo -n "#? "
+        read a
+        case $a in
+                1) ./scriptStudent.sh ;;
+                2) ./scriptServer.sh ;;
+                3) ./scriptOld.sh  ;;
+                4) ./scriptUpdate.sh ; menu ;;
+                5) ./updateScriptFiles.sh ;;
+                        0) exit 0 ;;
+                        *) echo -e $red"V‰‰r‰ valinta!"$clear;sleep 1; menu;;
+        esac
+}
+
+
+menu
+
