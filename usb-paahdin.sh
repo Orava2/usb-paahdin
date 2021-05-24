45ce7af7f7ac88e11380727ed535905ac91e58bb
Orava2 <32876893+Orava2@users.noreply.github.com>
2021-05-24 15:10:49 +0300

    Update usb-paahdin.sh

--- usb-paahdin.sh
+++ usb-paahdin.sh
@@ -11,7 +11,7 @@ Valitse toiminto
  2) Kirjoita palvelintikuiksi
  3) Kirjoita vanha versio
  4) Tarkista levykuvien päivitykset
- 5) Tarkista USB-paahtimen päivitykset
+ 9) Tarkista USB-paahtimen päivitykset
  0) Lopeta\n"
 echo -n "#? "
         read a
@@ -20,7 +20,7 @@ echo -n "#? "
                 2) ./scriptServer.sh ;;
                 3) ./scriptOld.sh  ;;
                 4) ./scriptUpdate.sh ; menu ;;
-                5) ./updateScriptFiles.sh ;;
+                9) ./updateScriptFiles.sh ;;
                         0) exit 0 ;;
                         *) echo -e ${redBk}"Virheellinen valinta!"${end};sleep 1; menu;;
         esac
