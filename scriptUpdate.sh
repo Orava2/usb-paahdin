9de4fb6b9b8b07bff854c061732624bdb94d6e0c
Orava2 <32876893+Orava2@users.noreply.github.com>
2021-05-24 11:30:32 +0300

    Update scriptUpdate.sh

--- scriptUpdate.sh
+++ scriptUpdate.sh
@@ -3,6 +3,7 @@ clear
 # Otetaan talteen se hakemistopolku, jossa ajossa oleva skripti sijaitsee. Esim. /home/user/usb-paahdin
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo -e "\e[1m\e[7m  ABITTI-tikkuohjelma aka USB-paahdin  \e[0m"
 echo "Kumpi levykuva päivitetään?"
 select yn in "Opiskelijan tikut" "Palvelimen tikut"; do
 	case $yn in
