effaa8776df017f6bc4203f6622ed17175f55e39
Orava2 <32876893+Orava2@users.noreply.github.com>
2021-05-25 09:11:43 +0300

    Update updateScriptFiles.sh

--- updateScriptFiles.sh
+++ updateScriptFiles.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 echo "Checking changes..."
 git reset --hard
 git pull
 echo ""
 bash allowExecution.sh
 
-echo -e "\nP‰ivitykset tarkistettu. Lopeta painamalla ENTER-n‰pp‰int‰."
+echo -e "\nP√§ivitykset tarkistettu. Lopeta painamalla ENTER-n√§pp√§int√§."
 read
