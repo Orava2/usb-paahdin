6575c3bb60b9c864230dfc17d60f06c9e2a4162c
Orava2 <32876893+Orava2@users.noreply.github.com>
2022-11-07 12:35:51 +0200

    Update

--- allowExecution.sh
+++ allowExecution.sh
@@ -29,7 +29,7 @@ echo "chmod a+x updateScriptFiles.sh"
 chmod a+x restoreSticks.sh
 echo "chmod a+x restoreSticks.sh"
 
-chmod +x usb-monster/src/dd_writer.py
-echo "chmod a+x usb-monster/src/dd_writer.py"
+chmod +x usb-monster/src/writer_dd.py
+echo "chmod a+x usb-monster/src/writer_dd.py"
 
 echo "Done"
